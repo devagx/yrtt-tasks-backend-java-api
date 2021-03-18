5cc657d93610afcf4f41508422edd84dcf9cceaf
devagx <alungroome@hotmail.com>
2021-03-18 17:20:03 +0000

    Created SaveTask functionality.

--- src/main/java/com/techreturners/SaveTaskHandler.java
+++ src/main/java/com/techreturners/SaveTaskHandler.java
@@ -4,41 +4,87 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techreturners.model.Task;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class SaveTaskHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	private static final Logger LOG = LogManager.getLogger(SaveTaskHandler.class);
+    private static final Logger LOG = LogManager.getLogger(SaveTaskHandler.class);
 
-	@Override
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-		LOG.info("received the request!!!!..");
+    private Connection connection = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
 
-		String userId = request.getPathParameters().get("userId");
-		String requestBody = request.getBody();
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+        LOG.info("received the request");
 
-		ObjectMapper objMapper = new ObjectMapper();
-		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-		response.setStatusCode(200);
+        String userId = request.getPathParameters().get("userId");
+        String requestBody = request.getBody();
 
-		{
-			try {
-				Task t = objMapper.readValue(requestBody, Task.class);
-				LOG.debug("Saved task: " + t.getDescription());
-				response.setBody("Task saved");
-			} catch (IOException e) {
-				LOG.error("Unable to unmarshall tasks array", e);
-			}
-		}
+        ObjectMapper objMapper = new ObjectMapper();
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setStatusCode(200);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Access-Control-Allow-Origin", "*");
+        response.setHeaders(headers);
 
-		return response;
-	}
+        try {
+            Task t = objMapper.readValue(requestBody, Task.class);
+
+            Class.forName("com.mysql.jdbc.Driver");
+
+            connection = DriverManager.getConnection(String.format("jdbc:mysql://%s/%s?user=%s&password=%s",
+                    System.getenv("DB_HOST"),
+                    System.getenv("DB_NAME"),
+                    System.getenv("DB_USER"),
+                    System.getenv("DB_PASSWORD")));
+
+            preparedStatement = connection.prepareStatement("INSERT INTO task VALUES (?, ?, ?, ?)");
+            preparedStatement.setString(1, UUID.randomUUID().toString());
+            preparedStatement.setString(2, userId);
+            preparedStatement.setString(3, t.getDescription());
+            preparedStatement.setBoolean(4, t.isCompleted());
+
+            preparedStatement.execute();
+
+            connection.close();
+        } catch (IOException e) {
+            LOG.error("Unable to unmarshal JSON for adding a task", e);
+        } catch (ClassNotFoundException e) {
+            LOG.error("ClassNotFoundException", e);
+        } catch (SQLException throwables) {
+            LOG.error("SQL Exception", throwables);
+        } finally {
+            closeConnection();
+        }
+
+        return response;
+    }
+
+    private void closeConnection() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception e) {
+            LOG.error("Unable to close connections to MySQL - {}", e.getMessage());
+        }
+    }
 }
